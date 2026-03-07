75f2b857e5cf71e0c76ca9d3c69868c69607606f
capinhoooo <232210033@student.ibik.ac.id>
2026-03-07 19:50:14 +0700

    fix: move Infura API key to env variable

--- web/src/lib/wagmi.ts
+++ web/src/lib/wagmi.ts
@@ -3,15 +3,16 @@ import { http } from 'wagmi'
 import { sepolia, baseSepolia, arbitrumSepolia, optimismSepolia } from 'wagmi/chains'
 
 const projectId = import.meta.env.VITE_WALLETCONNECT_PROJECT_ID || 'debor-dev'
+const infuraKey = import.meta.env.VITE_INFURA_KEY || ''
 
 export const wagmiConfig = getDefaultConfig({
   appName: 'DeBOR',
   projectId,
   chains: [sepolia, baseSepolia, arbitrumSepolia, optimismSepolia],
   transports: {
-    [sepolia.id]: http('https://sepolia.infura.io/v3/b6652bb1dac64d0a96a7a01043b44ee4'),
-    [baseSepolia.id]: http('https://base-sepolia.infura.io/v3/b6652bb1dac64d0a96a7a01043b44ee4'),
-    [arbitrumSepolia.id]: http('https://arbitrum-sepolia.infura.io/v3/b6652bb1dac64d0a96a7a01043b44ee4'),
-    [optimismSepolia.id]: http('https://optimism-sepolia.infura.io/v3/b6652bb1dac64d0a96a7a01043b44ee4'),
+    [sepolia.id]: http(`https://sepolia.infura.io/v3/${infuraKey}`),
+    [baseSepolia.id]: http(`https://base-sepolia.infura.io/v3/${infuraKey}`),
+    [arbitrumSepolia.id]: http(`https://arbitrum-sepolia.infura.io/v3/${infuraKey}`),
+    [optimismSepolia.id]: http(`https://optimism-sepolia.infura.io/v3/${infuraKey}`),
   },
 })
